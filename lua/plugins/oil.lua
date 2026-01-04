4e7f326b8604bd755cf5cad07b0417936f8b076a
Myo Thiha Kyaw <90295880+myothihakyaw-codigo@users.noreply.github.com>
2026-01-04 23:40:13 +0700

    added configuration for file navigation with oil nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  keys = {
+    { "<leader>e", "<cmd>lua require('oil').toggle_float()<CR>", desc = "Open Oil (Float)" },
+  },
+  opts = {
+    float = {
+      padding = 2,
+      max_width = 0,
+      max_height = 0,
+      border = "rounded",
+      win_options = {
+        winhl = "Normal:NormalFloat",
+      },
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
