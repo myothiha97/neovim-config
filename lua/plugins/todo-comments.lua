849288e94c922a5eba9e443a21858aae8d1c2887
Myo Thiha Kyaw <90295880+myothihakyaw-codigo@users.noreply.github.com>
2026-03-22 21:04:49 +0700

    feat: swap <leader>st/<leader>sT and extract keywords as shared variable
    - <leader>st now shows actionable todos (TODO/HACK/WARN)
    - <leader>sT now shows all todo keywords via vim.tbl_keys()
    - keywords table extracted as a local variable so opts and pickers stay in sync

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,23 +1,24 @@
+local keywords = {
+  TODO = { icon = " ", alt = { "TODO", "Todo", "Issue", "Bug" } },
+  PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "Perf", "Opt" } },
+  REFACTOR = { icon = " ", color = "default", alt = { "Refactor", "REF", "REFACTOR" } },
+  HACK = { icon = " ", color = "warning" },
+  WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+  NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+  TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+}
+
 return {
   {
     "folke/todo-comments.nvim",
     opts = {
-      keywords = {
-        TODO = { icon = " ", alt = { "TODO", "Todo", "Issue", "Bug" } },
-        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "Perf", "Opt" } },
-        REFACTOR = { icon = " ", color = "default", alt = { "Refactor", "REF", "REFACTOR" } },
-        HACK = { icon = " ", color = "warning" },
-        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-      },
+      keywords = keywords,
     },
     keys = {
       {
         "tt",
         function()
           require("todo-comments").jump_next()
-          --
         end,
         desc = "Next Todo",
       },
@@ -27,6 +28,23 @@ return {
           require("todo-comments").jump_prev()
         end,
         desc = "Previous Todo",
+      },
+      -- Swap defaults: <leader>st = specific (TODO/HACK/WARN), <leader>sT = all
+      { "<leader>st", false },
+      { "<leader>sT", false },
+      {
+        "<leader>st",
+        function()
+          Snacks.picker.todo_comments({ keywords = { "TODO", "HACK", "WARN" } })
+        end,
+        desc = "Todo/Fix/Fixme",
+      },
+      {
+        "<leader>sT",
+        function()
+          Snacks.picker.todo_comments({ keywords = vim.tbl_keys(keywords) })
+        end,
+        desc = "Todo (all)",
       },
     },
   },
