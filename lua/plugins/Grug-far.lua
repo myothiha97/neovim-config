fff313e0012697cd7198518c9322ea742b9e5a90
myothihakyaw <mthk97.dev@gmail.com>
2026-04-07 11:44:20 +0700

    feat(grug-far): add rename-word keymaps and hidden-file search
    - <leader>sF: project-wide search/replace now scans hidden files and
      ignores node_modules/.git/build
    - <leader>sR: rename word under cursor across the project
    - <leader>sr: rename word under cursor within the current file

--- lua/plugins/Grug-far.lua
+++ lua/plugins/Grug-far.lua
@@ -6,7 +6,7 @@ return {
     },
     keys = {
       {
-        "<leader>sr",
+        "<leader>sF",
         function()
           local grug = require("grug-far")
           local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
@@ -20,12 +20,12 @@ return {
           grug.open({
             prefills = {
               filesFilter = filter,
-              flags = "--ignore-case --sort=path",
+              flags = "--ignore-case --sort=path --hidden --no-ignore --glob !node_modules/ --glob !.git/ --glob !build/",
             },
           })
         end,
         mode = { "n", "x" },
-        desc = "Search and Replace (Grug-far)",
+        desc = "Search and Replace in Project (Grug-far)",
       },
       {
         "<leader>sf",
@@ -45,6 +45,51 @@ return {
         end,
         mode = { "n", "x" },
         desc = "Search and Replace in Current File (Grug-far)",
+      },
+      {
+        "<leader>sR",
+        function()
+          local grug = require("grug-far")
+          local word = vim.fn.expand("<cword>")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          local ext_groups = {
+            ts = "*.{ts,tsx}",
+            tsx = "*.{ts,tsx}",
+            js = "*.{js,jsx}",
+            jsx = "*.{js,jsx}",
+          }
+          local filter = ext_groups[ext] or ((ext and ext ~= "") and ("*." .. ext) or nil)
+          grug.open({
+            prefills = {
+              search = word,
+              filesFilter = filter,
+              flags = "--sort=path --hidden --no-ignore --glob !node_modules/ --glob !.git/ --glob !build/",
+            },
+          })
+        end,
+        mode = { "n" },
+        desc = "Rename Word Under Cursor in Project (Grug-far)",
+      },
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          local word = vim.fn.expand("<cword>")
+          local path = vim.fn.expand("%:p")
+          if path == "" then
+            return
+          end
+          grug.open({
+            prefills = {
+              search = word,
+              filesFilter = vim.fn.expand("%:t"),
+              paths = vim.fn.expand("%:h"),
+              flags = "--sort=path",
+            },
+          })
+        end,
+        mode = { "n" },
+        desc = "Rename Word Under Cursor in File (Grug-far)",
       },
     },
   },
